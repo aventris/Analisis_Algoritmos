ef9ab6860f6821f98f267092256730c3aa3dc8c6
Elioth Monroy <eliothmonroy@yahoo.com.mx>
2018-06-05 10:00:33 -0500

    Nada

---
+++ Proyecto/kmp.py
@@ -0,0 +1,13 @@
+def encontrar_f_fallos(cadena):
+	indice=0
+	fallos=[]
+	fallos.append(-1)
+	fallos.append(0)
+	for caracter in cadena:
+		pass
+	return fallos
+
+secuencia="CTCACTGCCTGCCTAG"
+patron="CTGCCTAG"
+funcion_fallo=encontrar_f_fallos(patron)
+print(funcion_fallo)

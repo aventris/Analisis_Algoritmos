9f8009afa97d91b5d2752a9f44007ecb128d8056
Elioth Monroy <eliothmonroy@yahoo.com.mx>
2018-06-04 23:38:36 -0500

    Nada

---
+++ Proyecto/proyecto.js
@@ -0,0 +1,18 @@
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+function main(){
+	var c = document.getElementById("myCanvas");
+	var ctx = c.getContext("2d");
+	ctx.moveTo(0,0);
+	ctx.lineTo(200,100);
+	ctx.stroke();
+	console.log("Debí de haber pintado");
+	ctx.strokeStyle="green";
+	ctx.stroke();
+}

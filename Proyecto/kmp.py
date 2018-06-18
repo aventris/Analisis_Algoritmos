e121f46d61bd6bac1d023aab709098514e53b8ff
Elioth Monroy <eliothmonroy@yahoo.com.mx>
2018-06-18 00:08:40 -0500

    Ya quedo?

--- Proyecto/kmp.py
+++ Proyecto/kmp.py
@@ -1,13 +1,53 @@
 def encontrar_f_fallos(cadena):
 	indice=0
+	comparar=0
 	fallos=[]
 	fallos.append(-1)
 	fallos.append(0)
 	for caracter in cadena:
-		pass
+		if indice==0 or (indice+1)==len(cadena):
+			pass
+		else:
+			# print("indice:"+cadena[indice])
+			# print("comparar:"+cadena[comparar])
+			# print(comparar)
+			if cadena[indice]==cadena[comparar]:
+				comparar+=1
+				fallos.append(comparar)
+			else:
+				while comparar>0:
+					comparar-=1
+					if cadena[indice]==cadena[comparar]:
+						comparar+=1
+						break
+				fallos.append(comparar)
+		indice+=1
 	return fallos
+
+def encontrar_cadena(secuencia,patron,funcion_fallo):
+	i=0
+	indice=0
+	while i<len(secuencia):
+		if secuencia[i]==patron[indice]:
+			if indice+1==len(patron):
+				return "Patron encontrado"
+			i+=1
+			indice+=1
+		else:
+			mover=funcion_fallo[indice]
+			if mover==-1:
+				indice=0
+				i+=1
+			if mover==0:
+				indice=0
+			if mover>0:
+				indice=mover
+	return "Patron no encontrado"
+
+
 
 secuencia="CTCACTGCCTGCCTAG"
 patron="CTGCCTAG"
 funcion_fallo=encontrar_f_fallos(patron)
 print(funcion_fallo)
+print(encontrar_cadena(secuencia,patron,funcion_fallo))
